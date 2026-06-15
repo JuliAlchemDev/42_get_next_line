8aaa3ac2c776128a8320c5a8fb26e32e900ccccc
Julia Alkhimova <devalchem@gmail.com>
2026-06-15 09:08:01 +0200

    chore: explore read() byte by byte

--- playground.c
+++ playground.c
@@ -3,13 +3,33 @@
 #include <stdio.h>
 
 // Discovering how use open & read functions   
-
+/*
 int main(void) {
     char buffer[20];
 
     int fd = open("text.txt", O_RDONLY);
     read(fd, buffer, 12);
     write(1, buffer, 12);
+    close(fd);
+    return (0);
+}
+*/
+
+// Reading text.txt byte by byte 
+
+int main(void) {
+    char buffer[20];
+    
+    int fd = open("text.txt", O_RDONLY);
+    int bytes_read = read(fd, buffer, 1);
+
+    printf("Bytes in buffer: %d\n", bytes_read); // 1
+
+    while(bytes_read > 0)
+    {
+    write(1, &buffer, 1);
+    bytes_read = read(fd, buffer, 1);
+    }
     close(fd);
     return (0);
 }
