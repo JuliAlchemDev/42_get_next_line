22d536c28ff3b9295ab9f7d43fa2d4a7112b1879
Julia Alkhimova <devalchem@gmail.com>
2026-06-17 18:22:11 +0200

    feat: implement ft_strchr to handle NULL safely

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -14,12 +14,35 @@ size_t ft_strlen(const char *str)
     return (i);
 }
 
+char *ft_strchr(const char *str, int ch)
+{
+    int i;
+
+    if(!str)
+        return (NULL);
+    i = 0;
+    while(str[i])
+    {
+        if(str[i] == (char)ch)
+            return ((char *)&str[i]);
+        i++;
+    }
+    if(str[i] == (char)ch)
+            return ((char *)&str[i]);
+    return (NULL);
+}
+
 int main()
 {
-    char *test_str;
-    test_str = "Twinkle, twinkle, little star,\nHow I wonder what you are!";
-    printf("Len of the str: %zu\n", ft_strlen(test_str)); // Len of the str: 57
-    test_str = NULL; 
-    printf("Len of the str: %zu\n", ft_strlen(test_str));
+    char *test_str_1;
+    char *test_str_2;
+
+    test_str_1 = "Twinkle, twinkle, little star,\nHow I wonder what you are!";
+    test_str_2 = NULL; 
+    printf("Len of the str: %zu\n", ft_strlen(test_str_1)); // Len of the str: 57 
+    printf("NULL  case:  %zu\n", ft_strlen(test_str_2));
+
+    printf("First occurrence of '\\n': %s\n", ft_strchr(test_str_1, '\n')); 
+    printf("NULL string case: %s\n", ft_strchr(test_str_2, '\n')); 
 }
 
