4ad861bbf0e8d31f88ec234de08ac66dff9cbd6c
Julia Alkhimova <devalchem@gmail.com>
2026-06-18 10:04:20 +0200

    feat: add gnl header to centralize declarations and includes

--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,22 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdlib.h>
+# include <stdlib.h> // mallloc, free, size_t
+# include <fcntl.h> // O_RDONLY flag
+# include <unistd.h> // read & close
 
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 50
+#endif
+
+char *get_next_line(int fd);
+
+// GNL Utils
+size_t ft_strlen(const char *str);
+char *ft_strchr(const char *str, int ch);
+void *ft_memcpy(void *dst, const void *src, size_t n);
+char *ft_strjoin(const char *str_1, const char *str_2);
+char *ft_substr(const char *str, unsigned int start, size_t len);
 
 #endif
