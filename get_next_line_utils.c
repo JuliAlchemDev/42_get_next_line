65d3268ca8b26546eda241a091aca61badc42cf8
Julia Alkhimova <devalchem@gmail.com>
2026-06-18 09:58:04 +0200

    feat: implement ft_substr for buffer splitting

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,9 +1,5 @@
 #include "get_next_line.h"
 
-// DELETE after test
-#include <stdio.h> 
-#include <string.h>
-
 size_t ft_strlen(const char *str)
 {
     int i;
@@ -71,6 +67,38 @@ char *ft_strjoin(const char *str_1, const char *str_2)
     return (join);
 }
 
+char *ft_substr(const char *str, unsigned int start, size_t len)
+{
+    char *substr;
+    size_t i;
+    size_t s_len;
+
+    if(!str)
+        return (NULL);
+    s_len = ft_strlen(str);
+    if(start >= s_len)
+        len = 0;
+    else if(len > s_len - start)
+        len = s_len - start;
+    substr = malloc(len + 1);
+    if(!substr)
+        return (NULL);
+    i = 0;
+    while(i < len)
+    {
+        substr[i] = str[start + i];
+        i++;
+    }
+    substr[i] = '\0';
+    return (substr);   
+}
+
+/*
+#include <stdio.h> 
+
+#define PURPLE      "\033[38;5;93m"
+#define RESET     "\033[0m"
+
 int main()
 {
     char *test_str_1;
@@ -79,17 +107,24 @@ int main()
 
     test_str_1 = "Twinkle, twinkle, little star,\nHow I wonder what you are!";
     test_str_2 = NULL; 
-    printf("Len of the str: %zu\n", ft_strlen(test_str_1)); // Len of the str: 57 
-    printf("NULL  case:  %zu\n", ft_strlen(test_str_2)); // NULL  case:  0
+    printf(PURPLE "Len of the str:" RESET " %zu\n", ft_strlen(test_str_1)); // Len of the str: 57 
+    printf(PURPLE "NULL  case:" RESET " %zu\n", ft_strlen(test_str_2)); // NULL  case:  0
 
-    printf("First occurrence of '\\n': %s\n", ft_strchr(test_str_1, '\n')); // First occurrence of '\n': How I wonder what you are!
-    printf("NULL string case: %s\n", ft_strchr(test_str_2, '\n')); // NULL string case: (null)
+    printf(PURPLE "First occurrence of '\\n':" RESET " %s\n", ft_strchr(test_str_1, '\n')); // First occurrence of '\n': How I wonder what you are!
+    printf(PURPLE "NULL string case:" RESET " %s\n", ft_strchr(test_str_2, '\n')); // NULL string case: (null)
 
     result = ft_strjoin("Twinkle, twinkle,", " little star,");
-    printf("Join: %s\n", result); // Join: Twinkle, twinkle, little star,
+    printf(PURPLE "Join:" RESET " %s\n", result); // Join: Twinkle, twinkle, little star,
     free(result);
     result = ft_strjoin("Twinkle, twinkle,", NULL);
-    printf("Join with NULL: %s\n", result); // Join with NULL: (null)
+    printf(PURPLE "Join with NULL:" RESET " %s\n", result); // Join with NULL: (null)
+    free(result);
+
+    result  = ft_substr(test_str_1, 25, 4); 
+    printf(PURPLE "Extract 'star' from string 1:" RESET " %s\n", result); 
+    free(result);
+    result  = ft_substr(NULL, 25, 4); 
+    printf(PURPLE "Test ft_substr with NULL as a string:" RESET " %s\n", result); 
     free(result);
 }
-
+*/
